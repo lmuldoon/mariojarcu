4a794f1a588da83c02db7d534ba0545c05bf300c
Luke Muldoon <LukeMuldoon@core-marketing.co.uk>
2026-07-01 16:08:51 +0100

    Update files

--- wp-content/themes/mariojarcu/footer.php
+++ wp-content/themes/mariojarcu/footer.php
@@ -53,7 +53,7 @@
         </div>
 
         <!-- Bottom bar -->
-        <div class="site-footer__bottom">
+        <div class="site-footer__bottom animated">
             <p class="site-footer__copy">&copy; <?php
 $year = (int) date('Y');
 $romans = [1000=>'M',900=>'CM',500=>'D',400=>'CD',100=>'C',90=>'XC',50=>'L',40=>'XL',10=>'X',9=>'IX',5=>'V',4=>'IV',1=>'I'];
